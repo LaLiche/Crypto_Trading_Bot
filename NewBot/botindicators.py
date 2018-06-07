fe0021343c9e3ac1bd07395c4eabc329aef7cae6
pieta <adrien.piet@phelma.genoble-inp.fr>
2018-06-07 22:22:21 +0200

    ajout du graphe RSI

--- NewBot/botindicators.py
+++ NewBot/botindicators.py
@@ -25,17 +25,19 @@ class BotIndicators(object):
 	def MACD(self, prices, nslow=26, nfast=12):
 		emaslow = self.EMA(prices, nslow)
 		emafast = self.EMA(prices, nfast)
-		return emaslow, emafast, emafast - emaslow		
+		return emaslow, emafast, emafast - emaslow
 
 	def RSI (self, prices, period=14):
-		deltas = np.diff(prices)
+		deltas = numpy.diff(prices)
 		seed = deltas[:period+1]
 		up = seed[seed >= 0].sum()/period
 		down = -seed[seed < 0].sum()/period
-		rs = up/down
-		rsi = np.zeros_like(prices)
+		if down != 0:
+			rs = up/down
+		else:
+			rs = 1
+		rsi = numpy.zeros_like(prices)
 		rsi[:period] = 100. - 100./(1. + rs)
- 
 		for i in range(period, len(prices)):
  			delta = deltas[i - 1]  # cause the diff is 1 shorter
   			if delta > 0:
@@ -44,7 +46,7 @@ class BotIndicators(object):
  			else:
  				upval = 0.
  				downval = -delta
- 
+
  			up = (up*(period - 1) + upval)/period
  			down = (down*(period - 1) + downval)/period
   			rs = up/down
--- NewBot/plotgraphe.py
+++ NewBot/plotgraphe.py
@@ -2,22 +2,59 @@ import plotly
 from botchart import BotChart
 from poloniex import poloniex
 import timetranslate as tt
+from botindicators import BotIndicators
 
 
 class PlotGraphe(object):
     def __init__(self,chart):
         self.chart = chart
+        self.indicateurs = BotIndicators()
+
+
+    def plotrsi(self,prices,temps):
+
+        RSI_data = [50]
+        for i in range(1,len(prices)):
+            RSI_data.append(self.indicateurs.RSI(prices[:i]))
+        RSI_data.append(50)
+
+        rsi = plotly.graph_objs.Scatter(
+        x = temps,
+        y = RSI_data,
+        )
+
+        rsi_min = plotly.graph_objs.Scatter(
+        x = temps,
+        y = [30 for i in range(len(temps))]
+        )
+
+        rsi_max = plotly.graph_objs.Scatter(
+        x = temps,
+        y = [70 for i in range(len(temps))]
+        )
+
+        return rsi,rsi_min,rsi_max
+
+
+
+    def plotcandle(self,open_data,close_data,high_data,low_data,x_data):
+
+        trace = plotly.graph_objs.Candlestick(
+            x = x_data,
+            open = open_data,
+            close = close_data,
+            high = high_data,
+            low = low_data)
+        return trace
 
     def plotchart(self):
 
-        # TODO formater la date + ajouter strategies & indicateurs
-
+        # TODO ajouter strategies & indicateurs
         open_data = []
         close_data = []
         high_data = []
         low_data = []
         x_data = []
-
 
         for c in self.chart.data:
             open_data.append(c.open)
@@ -26,19 +63,21 @@ class PlotGraphe(object):
             low_data.append(c.low)
             x_data.append(c.startTime)
 
-        trace = plotly.graph_objs.Candlestick(
-            x = x_data,
-            open = open_data,
-            close = close_data,
-            high = high_data,
-            low = low_data)
+        trace = self.plotcandle(open_data,close_data,high_data,low_data,x_data)
+        rsi,rsi_min,rsi_max = self.plotrsi(close_data,x_data)
 
         layout = {
             'title': self.chart.pair+" "+str(self.chart.period)+" s",
-            'yaxis': {'title': self.chart.pair},
-             "xaxis" : {'ticks':"",'showticklabels':False,'ticktext': tt.FloattoTime(tab=x_data),'tickvals': x_data }
+            'yaxis1': {'title': self.chart.pair,'domain':[0.26,1]},
+            'xaxis' : {'ticks':"",'showticklabels':False,'ticktext': tt.FloattoTime(tab=x_data),'tickvals': x_data, },
+            'yaxis2':{'domain':[0,0.25]},
+            'xaxis2' : {'ticks':"",'showticklabels':False,'ticktext': tt.FloattoTime(tab=x_data),'tickvals': x_data, },
             }
 
-        data = [trace]
-        fig = dict(data=data, layout=layout)
+        fig = plotly.tools.make_subplots(rows=2, cols=1,shared_xaxes=True)
+        fig.append_trace(trace, 1, 1)
+        fig.append_trace(rsi, 2, 1)
+        fig.append_trace(rsi_min, 2, 1)
+        fig.append_trace(rsi_max, 2, 1)
+        fig['layout']=layout
         plotly.offline.plot(fig,filename='test_graphe.html')
